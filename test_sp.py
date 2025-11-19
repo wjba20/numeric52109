8969d1b73d70b679a8c72a4c932195ebb7c0529c
James Foadi <james_foadi@yahoo.co.uk>
2025-11-19 03:29:17 +0000

    First Commit.

---
+++ test_sp.py
@@ -0,0 +1,17 @@
+###
+## Test file for the package simple_package
+## Execute as 'python test_sp.py'
+###
+
+import simple_package as sp
+
+if __name__ == '__main__':
+    ## Define two numbers
+    a = 1;
+    b = 2;
+    
+    ## Print their sum with a nice message.
+    print(f"The sum of {a} and {b} is {a + b}")
+
+    ## Now do the same for the function in sp
+    print(f"The product of {a} and {b} is {sp.add(a,b)}")
