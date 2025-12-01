fc1e289c5b242df52736d397090fe58e82c69ed4
William Avery <wjba20@bath.ac.uk>
2025-12-01 02:42:57 +0000

    First task: fixed error in test_sp.py

--- test_sp.py
+++ test_sp.py
@@ -4,14 +4,15 @@
 ###
 
 import simple_package as sp
+import simple_package.operations as ops
 
 if __name__ == '__main__':
     ## Define two numbers
-    a = 1;
-    b = 2;
+    a = 1
+    b = 2
     
     ## Print their sum with a nice message.
-    print(f"The sum of {a} and {b} is {a + b}")
+    print(f"The sum of {a} and {b} is {ops.add(a, b)}")
 
     ## Now do the same for the function in sp
-    print(f"The product of {a} and {b} is {sp.add(a,b)}")
+    print(f"The product of {a} and {b} is {ops.multiply(a, b)}")
